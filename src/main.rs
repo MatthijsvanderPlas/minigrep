97d7d67a1142b9d66999cd20f802d8884891d2da
Matthijs van der Plas <mvdplas84@ziggo.nl>
2023-10-22 16:46:36 +0200

    Structs and impl methods

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,42 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool{
+        self.width > other.width && self.height > other.height
+    }
+    
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
-    let mut s = String::from("hello");
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
 
-    let r1 = &mut s;
-    let r2 = &mut s;
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
 
-    println!("{}, {}", r1, r2);
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    let sqr = Rectangle::square(5);
+
+    println!("My square is {:?}", sqr);
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
