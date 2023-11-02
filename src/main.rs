349d13a3b9db6133769fed1adecbc69df264e1f1
Matthijs van der Plas <mvdplas84@ziggo.nl>
2023-11-02 20:59:28 +0100

    On to error handling

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,32 @@
-use std::collections::HashMap;
+use std::fs::File;
+use std::io::{Read, Result};
 
 fn main() {
-    let text = "hello world wonderful world";
-
-    let mut map = HashMap::new();
-
-    for word in text.split_whitespace() {
-        let count = map.entry(word).or_insert(0);
-        *count += 1;
+    let result = read_line_from_file();
+    let mut count = 0;
+    match result {
+        Ok(lines) => {
+            for line in lines.trim().split("\n") {
+                count += 1;
+                println!("{} {}", count, line);
+            }
+        },
+        Err(e) => panic!("Something went wrong {}", e),
     }
+}
 
-    println!("{:?}", map);
+fn read_line_from_file() -> Result<String> {
+    let greeting_file_result = File::open("greeting.txt");
+
+    let mut greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut line = String::new();
+
+    match greeting_file.read_to_string(&mut line) {
+        Ok(_) => Ok(line),
+        Err(e) => Err(e),
+    }
 }
